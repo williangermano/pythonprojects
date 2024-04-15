1a1d517a2c0c7ad01ae6127602252ac10f53fc22
williangermano <156610948+williangermano@users.noreply.github.com>
2024-04-15 17:16:46 -0300

    Add files via upload

---
+++ BandGenerator.py
@@ -0,0 +1,7 @@
+#Day 1 pt-br
+
+print("Bem-vindo(a) ao Gerador de Nomes para Banda.")
+city = input("Qual é o nome da cidade em que você nasceu?")
+pet = input("Qual é o nome do seu pet?")
+print("O nome da sua banda poderia ser " + city + " " + pet )
+
